10dc7792e5939f00f88d7d6798bf79d965a4ccfe
Gabriel Coronadogit config --global user.name Gabriel <gecoronados@gmail.com>
2021-05-28 11:36:35 -0400

    Created Homepage.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./assets/launch.png";
 // import styled from "styled-components";
 import NavBar from "./components/NavBar";
+import Homepage from "./components/Homepage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/">
-            Home
+            <Homepage />
           </Route>
           <Route exact path="/about">
             About
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Gabriel's Portfolio begins here... at last</h1>
+    </div>
+  );
+};
+
+export default Homepage;
